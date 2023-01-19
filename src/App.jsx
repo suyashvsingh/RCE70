3833d9e7d2110b5d2cf95b8a1e7baf4934fa07c3
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2023-01-19 16:51:15 +0530

    Integrated backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css";
+import axios from "axios";
 
 const App = () => {
-  return <div>App</div>;
+  const [code, setCode] = useState("");
+  const [result, setResult] = useState("");
+
+  const onClickSubmit = async () => {
+    let headersList = {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+    };
+
+    let bodyContent = JSON.stringify({
+      code,
+    });
+
+    let reqOptions = {
+      url: "http://157.245.104.238/v1/code/exe-JS",
+      method: "POST",
+      headers: headersList,
+      data: bodyContent,
+    };
+
+    let response = await axios.request(reqOptions);
+    setResult(response.data.data);
+  };
+
+  return (
+    <div className="p-4 bg-slate-400 min-h-screen flex flex-col justify-between">
+      <div className="flex basis-[50%] justify-between items-start">
+        <Editor
+          className="bg-white"
+          value={code}
+          onValueChange={(code) => setCode(code)}
+          highlight={(code) => highlight(code, languages.js)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+            width: "50%",
+            height: "80vh",
+          }}
+        />
+        <div className="w-[100%] bg-white">{result}</div>
+      </div>
+      <button
+        className="bg-green-700 px-4 py-2 text-white rounded-md font-medium text-sm mt-4 w-fit ml-auto"
+        onClick={onClickSubmit}
+      >
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default App;
