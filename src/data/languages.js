c507c4af72f161ffd1d09e4321764c65135aae4c
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2023-01-26 14:57:17 +0530

    Added C and Go

--- src/data/languages.js
+++ src/data/languages.js
@@ -2,6 +2,8 @@ const languages = [
   { value: "js", label: "JavaScript" },
   { value: "py", label: "Python" },
   { value: "cpp", label: "C++" },
+  { value: "c", label: "C" },
+  { value: "go", label: "Go" },
 ];
 
 export default languages;
