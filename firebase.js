f6ef6fb05fe818ea3e91eade97f63d4c66ea57bb
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2023-03-11 21:26:30 +0530

    Configured firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZlT1rtSUyd7MgFdeqtdsMkE38FiffzN4",
+  authDomain: "rce70-616fc.firebaseapp.com",
+  projectId: "rce70-616fc",
+  storageBucket: "rce70-616fc.appspot.com",
+  messagingSenderId: "401904107771",
+  appId: "1:401904107771:web:384ae432742ef928f9305e",
+};
+
+const app = initializeApp(firebaseConfig);
